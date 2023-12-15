6d3bdccf9e062cd88b4cc25e9c41984668bdcc81
rikiosuzuki <suzuki.r@northeastern.edu>
2023-12-15 09:09:23 -0500

    sign in and sign up

--- kanbas-node-server-app/app.js
+++ kanbas-node-server-app/app.js
@@ -1,6 +1,5 @@
-
-
-import express from 'express';
+import session from "express-session";
+import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
@@ -9,18 +8,14 @@ import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
-import session from "express-session";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-
-mongoose.connect(CONNECTION_STRING);
-
-const app = express()
-app.use(cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL
-}));
-
+const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
 const sessionOptions = {
     secret: "any string",
     resave: false,
@@ -35,16 +30,17 @@ if (process.env.NODE_ENV !== "development") {
 }
 app.use(session(sessionOptions));
 
-
+// app.use(cors)
 app.use(express.json());
 UserRoutes(app);
-ModuleRoutes(app)
+ModuleRoutes(app);
 CourseRoutes(app);
-
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT ||4000)
+app.listen(process.env.PORT || 4000);
+app.use(express.json());
 
 
-
-
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/a6project'
+console.log(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
--- sports-statistics-site-node/Users/dao.js
+++ sports-statistics-site-node/Users/dao.js
@@ -1,11 +1,27 @@
+import { userModel, postModel } from "./model.js";
 
-import model from "./model.js";
+export const createUser = (user) => userModel.create(user);
+export const findAllUsers = () => userModel.find();
+export const findUserById = (userId) => userModel.findById(userId);
+export const findUserByUsername = async (username) => {
+  try {
+    const user = await userModel.findOne({ username: username });
+    return user;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
 export const findUserByCredentials = (username, password) =>
-    model.findOne({ username, password });
+    userModel.findOne({ username, password });
+export const updateUser = (userId, user) =>
+    userModel.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => userModel.deleteOne({ _id: userId });
 
-export const findUserByUsername = (username) =>
-    model.findOne({ username: username });
-
-export const createUser = (user) => model.create(user);
-export const findUserById = (userId) => model.findById(userId);
+export const createPost = (post) => postModel.create(post);
+export const findAllPosts = () => postModel.find();
+export const findPostById = (postId) => postModel.findById(postId);
+export const updatePost = (postId, post) =>
+    postModel.updateOne({ _id: postId }, { $set: post });
+export const deletePost = (postId) => postModel.deleteOne({ _id: postId });
